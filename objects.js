c57332d478871c0cde9ed389b2e2b11f00ad0e02
RimmerW2000 <rimmerenmirandawijngaarden@outlook.com>
2022-01-09 14:28:54 +0100

    opdracht hfst 3 abjects.js af op vraag 4 na lastig uit te komen

--- objects.js
+++ objects.js
@@ -1,11 +1,20 @@
 // ===  Opdracht 1 ===
 // Maak een object met daarin jouw voornaam (string), achternaam (string), leeftijd (getal) en geboortedatum (date object). De datum hoeft niet geformat te worden.
 // Log dit in de terminal.
-
+const itsMeRimmer = {
+  name: "Rimmer",
+  lastName: "Wijngaarden",
+  age: "21",
+  dateOfBirth: "24-3-2000",
+};
+console.log(itsMeRimmer)
 
 // ===  Opdracht 2 ===
 // Zorg ervoor dat de volgende string in de terminal wordt gelogd:
 // "Hoi! Ik ben <voornaam>, ik ben <leeftijd> jaar oud."
+const meItIs = ("Hoi! Ik ben Rimmer, ik ben 21 jaar oud.")
+console.log(meItIs)
+
 
 
 // === Opdracht 3: bekijk het volgende object ===
@@ -18,6 +27,12 @@ const product = {
   wireless: false,
   "b-h-d": "17 x 24,6 x 22,3 cm",
 };
+console.log(product.series);
+product.color = "roze";
+console.log(product.color);
+console.log(product.material[1]);
+console.log(product["b-h-d"]);
+
 
 // 3a. Log de naam van de serie in de terminal.
 
@@ -32,6 +47,9 @@ const product = {
 // Declareer een variabele met daarin een string die het product beschrijft. Gebruik hiervoor de properties productName en material uit het object.
 // ---- Verwachte uitkomst:
 // "De Smeg Waterkoker is gemaakt van roestvrijstaal en kunststof"
+const discription = {
+  "De ${product.productName} is gemaakt van ${product.material[0]} en ${product.material[1]}."
+}
 
 
 
